3b399ea61d4ee7a8c01967940782c30c1e434648
Eric Zimmerman <saericzimmerman@gmail.com>
2020-08-21 12:55:54 -0400

    Prod key

---
+++ RegistryPlugin.ProductKey/ValuesOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistryPluginBase.Interfaces;
+
+namespace RegistryPlugin.ProductKey
+{
+   public class ValuesOut:IValueOut
+    {
+
+        public ValuesOut(string productKey)
+        {
+            ProductKey = productKey;
+        }
+        public string ProductKey { get; set; }
+        public string BatchKeyPath { get; set; }
+        public string BatchValueName { get; set; }
+        public string BatchValueData1 => $"Product Key: {ProductKey}";
+        public string BatchValueData2 { get; }
+        public string BatchValueData3 { get; }
+    }
+}
