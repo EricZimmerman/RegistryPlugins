0316032c552fcedac47dfc568deff56945597858
rathbuna <36825567+rathbuna@users.noreply.github.com>
2021-02-13 15:37:45 -0500

    Update ValuesOut.cs

--- RegistryPlugin.Bam/ValuesOut.cs
+++ RegistryPlugin.Bam/ValuesOut.cs
@@ -15,7 +15,7 @@ namespace RegistryPlugin.BamDam
         public string BatchKeyPath { get; set; }
         public string BatchValueName { get; set; }
         public string BatchValueData1 => $"Program: {Program}";
-        public string BatchValueData2 =>$"Execution time: {ExecutionTime.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff})";
+        public string BatchValueData2 => $"Execution time: {ExecutionTime.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff})";
         public string BatchValueData3 { get; }
     }
 }
