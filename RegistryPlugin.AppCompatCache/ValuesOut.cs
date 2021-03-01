e9e395e24904119596451fc3611317a1526556f9
Andrew Rathbun <36825567+rathbuna@users.noreply.github.com>
2021-03-01 15:13:15 -0500

    Update ValuesOut.cs

--- RegistryPlugin.AppCompatCache/ValuesOut.cs
+++ RegistryPlugin.AppCompatCache/ValuesOut.cs
@@ -18,7 +18,7 @@ namespace RegistryPlugin.AppCompatCache
         public string BatchKeyPath { get; set; }
         public string BatchValueName { get; set; }
         public string BatchValueData1 => $"{ProgramName}";
-        public string BatchValueData2 => $"Modified: {ModifiedTime.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff})";
+        public string BatchValueData2 => $"Modified: {ModifiedTime.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff}";
         public string BatchValueData3 => $"Position: {CacheEntryPosition}";
     }
 }
