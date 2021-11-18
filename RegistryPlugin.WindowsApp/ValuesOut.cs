4efe0609d3677d45eef85cfa9bc48399f447e8e9
Hyun Yi <hy00un__@naver.com>
2021-11-18 17:13:08 +0900

    add WindowsApp Plugin

---
+++ RegistryPlugin.WindowsApp/ValuesOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RegistryPluginBase.Interfaces;
+
+namespace RegistryPlugin.WindowsApp
+{
+    public class ValuesOut : IValueOut
+    {
+        public ValuesOut(string displayname, DateTimeOffset? installtime, string packagerootfolder)
+        {
+            DisplayName = displayname;
+            InstallTime = installtime;
+            PackageRootFolder = packagerootfolder;
+        }
+
+        public string DisplayName { get; }
+        public DateTimeOffset? InstallTime { get; }
+        public string PackageRootFolder { get; }
+        public string BatchKeyPath { get; set; }
+        public string BatchValueName { get; set; }
+        public string BatchValueData1 => $"DisplayName: {DisplayName} PackageRootFolder: {PackageRootFolder}";
+        public string BatchValueData2 => $"InstallTime: {InstallTime?.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff}";
+        public string BatchValueData3 => string.Empty;
+    }
+}
