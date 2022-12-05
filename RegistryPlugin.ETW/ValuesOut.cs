b511fd8815b134415e8d35de9def58eb43f74f1f
Mohammed Hassan <51376376+0xHasanM@users.noreply.github.com>
2022-12-05 14:13:48 +0200

    Add files via upload

---
+++ RegistryPlugin.ETW/ValuesOut.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RegistryPluginBase.Interfaces;
+
+namespace RegistryPlugin.ETW
+{
+    public class ValuesOut : IValueOut
+    {
+        public ValuesOut(string lastwritetimestamp, string guid, string provider, string enabled, string enabledproperty)
+        {
+            LastWriteTimestamp = lastwritetimestamp;
+            Guid = guid;
+            Provider = provider;
+            Enabled = enabled;
+            EnabledProperty = enabledproperty;
+        }
+
+        public string LastWriteTimestamp { get; }
+        public string Guid { get; }
+        public string Provider { get; }
+        public string Enabled { get; }
+        public string EnabledProperty { get; }
+        public string BatchKeyPath { get; set; }
+        public string BatchValueName { get; set; }
+        public string BatchValueData1 => string.Empty;
+        public string BatchValueData2 => string.Empty;
+        public string BatchValueData3 => string.Empty;
+    }
+}
