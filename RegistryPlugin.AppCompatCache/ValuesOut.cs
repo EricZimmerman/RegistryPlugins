fd3b2d1e081d3524feb12f61fb04f8392519f642
Eric Zimmerman <saericzimmerman@gmail.com>
2023-03-07 19:43:33 -0500

    Dependencies

--- RegistryPlugin.AppCompatCache/ValuesOut.cs
+++ RegistryPlugin.AppCompatCache/ValuesOut.cs
@@ -5,20 +5,22 @@ namespace RegistryPlugin.AppCompatCache
 {
     public class ValuesOut:IValueOut
     {
-        public ValuesOut(int cacheEntryPosition, string programName, DateTimeOffset modDate)
+        public ValuesOut(int cacheEntryPosition, string programName, DateTimeOffset? modDate)
         {
             CacheEntryPosition = cacheEntryPosition;
             ProgramName = programName;
-            ModifiedTime = modDate.UtcDateTime;
+           
+            ModifiedTime = modDate?.UtcDateTime;
+            
         }
 
         public int CacheEntryPosition { get; }
         public string ProgramName { get; }
-        public DateTime ModifiedTime { get; }
+        public DateTime? ModifiedTime { get; }
         public string BatchKeyPath { get; set; }
         public string BatchValueName { get; set; }
         public string BatchValueData1 => $"{ProgramName}";
-        public string BatchValueData2 => $"Modified: {ModifiedTime.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff}";
+        public string BatchValueData2 => $"Modified: {ModifiedTime?.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff}";
         public string BatchValueData3 => $"Position: {CacheEntryPosition}";
     }
 }
