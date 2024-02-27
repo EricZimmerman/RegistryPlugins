8d2e3cab238a8876076f5b91354525fbdadb3643
Hyun Yi <hy00un__@naver.com>
2024-02-27 16:03:05 +0900

    add data (LeaseObtainedTime, LeaseTerminatesTime)

--- RegistryPlugin.NetworkSettings/ValuesOut.cs
+++ RegistryPlugin.NetworkSettings/ValuesOut.cs
@@ -5,7 +5,7 @@ namespace RegistryPlugin.NetworkSettings
 {
     public class ValuesOut : IValueOut
     {
-        public ValuesOut(string ipaddress, string subnetmask, string dhcpsubnetmask, string dhcpserver, string dhcpnameserver, string dhcpipaddress, string dhcpdefaultgateway, bool enabledhcp)
+        public ValuesOut(string ipaddress, string subnetmask, string dhcpsubnetmask, string dhcpserver, string dhcpnameserver, string dhcpipaddress, string dhcpdefaultgateway, DateTimeOffset? leaseobtainedtime, DateTimeOffset? leaseterminatestime, bool enabledhcp)
         {
             IPAddress = ipaddress;
             SubnetMask = subnetmask;
@@ -14,6 +14,8 @@ namespace RegistryPlugin.NetworkSettings
             DHCPNameServer = dhcpnameserver;
             DHCPIPAddress = dhcpipaddress;
             DHCPDefaultGateway = dhcpdefaultgateway;
+            LeaseObtainedTime = leaseobtainedtime?.UtcDateTime;
+            LeaseTerminatesTime = leaseterminatestime?.UtcDateTime;
             EnabledDHCP = enabledhcp;
         }
 
@@ -24,12 +26,14 @@ namespace RegistryPlugin.NetworkSettings
         public string DHCPNameServer { get; }
         public string DHCPIPAddress { get; }
         public string DHCPDefaultGateway { get; }
+        public DateTime? LeaseObtainedTime { get; }
+        public DateTime? LeaseTerminatesTime { get; }
         public bool EnabledDHCP { get; }
         public string BatchKeyPath { get; set; }
         public string BatchValueName { get; set; }
         public string BatchValueData1 => $"IPAddress: {IPAddress} SubnetMask: {SubnetMask}";
         public string BatchValueData2 => $"DHCPSubnetMask: {DHCPSubnetMask} DHCPServer: {DHCPServer} DHCPNameServer: {DHCPNameServer} DHCPIPAddress: {DHCPIPAddress} DHCPDefaultGateway: {DHCPDefaultGateway}";
-        public string BatchValueData3 => $"EnabledDHCP: {EnabledDHCP}";
+        public string BatchValueData3 => $"LeaseObtainedTime: {LeaseObtainedTime?.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff} LeaseTerminatesTime: {LeaseTerminatesTime?.ToUniversalTime():yyyy-MM-dd HH:mm:ss.fffffff} EnabledDHCP: {EnabledDHCP}";
     }
 }
 
