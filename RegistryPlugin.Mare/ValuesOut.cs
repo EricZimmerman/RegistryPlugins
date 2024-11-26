1f16b443a392b348e50c0c95e8fbc5166235bf84
Hyun Yi <hy00un__@naver.com>
2024-11-26 23:13:48 +0900

    add Mare Plugin

---
+++ RegistryPlugin.Mare/ValuesOut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RegistryPluginBase.Interfaces;
+
+namespace RegistryPlugin.Mare
+{
+    public class ValuesOut : IValueOut
+    {
+        public ValuesOut(string unknown, string path)
+        {
+            Unknown = unknown;
+            Path = path;
+        }
+
+        public string Unknown { get; }
+        public string Path { get; }
+
+        public string BatchKeyPath { get; set; }
+        public string BatchValueName { get; set; }
+        public string BatchValueData1 => $"Unknown: {Unknown}";
+        public string BatchValueData2 => $"Path: {Path}";
+        public string BatchValueData3 => $"";
+    }
+}
