eac9a41ee57f646f24010658b3c812d2e75ef3c8
Hyun Yi <hy00un__@naver.com>
2024-12-18 21:18:29 +0900

    Update Products.cs

--- RegistryPlugin.Products/Products.cs
+++ RegistryPlugin.Products/Products.cs
@@ -21,7 +21,8 @@ namespace RegistryPlugin.Products
 
         public List<string> KeyPaths => new List<string>(new[]
         {
-            @"Microsoft\Windows\CurrentVersion\Installer\UserData\*\Products"
+            @"Microsoft\Windows\CurrentVersion\Installer\UserData\*\Products",
+            @"Classes\Installer\Products"
         });
 
         public string ValueName => null;
@@ -57,34 +58,70 @@ namespace RegistryPlugin.Products
         {
             var l = new List<ValuesOut>();
 
-            foreach (var subKey in key.SubKeys)
+            if (key.KeyPath.Contains("Classes"))
             {
-                try
+                foreach (var subKey in key.SubKeys)
                 {
-                    var properties = subKey.SubKeys.SingleOrDefault(t => t.KeyName == "InstallProperties");
-                    if (properties == null)
-                        continue;
-
-                    var displayName = properties.Values.SingleOrDefault(t => t.ValueName == "DisplayName")?.ValueData;
-                    var packageName = properties.Values.SingleOrDefault(t => t.ValueName == "DisplayVersion")?.ValueData;
-                    var installDate = properties.Values.SingleOrDefault(t => t.ValueName == "InstallDate")?.ValueData;
-                    var publisher = properties.Values.SingleOrDefault(t => t.ValueName == "Publisher")?.ValueData;
-                    var language = properties.Values.SingleOrDefault(t => t.ValueName == "Language")?.ValueData;
-                    var installLocation = properties.Values.SingleOrDefault(t => t.ValueName == "InstallLocation")?.ValueData;
-                    var installSource = properties.Values.SingleOrDefault(t => t.ValueName == "InstallSource")?.ValueData;
-                    var comments = properties.Values.SingleOrDefault(t => t.ValueName == "Comments")?.ValueData;
-                    DateTimeOffset? ts = properties.LastWriteTime;
-
-                    var ff = new ValuesOut(displayName, packageName, installDate, publisher, language, installLocation, installSource, comments, ts)
+                    try
                     {
-                        BatchValueName = "Multiple",
-                        BatchKeyPath = subKey.KeyPath
-                    };
-                    l.Add(ff);
+                        var productName = subKey.Values.SingleOrDefault(t => t.ValueName == "ProductName")?.ValueData;
+                        var language = subKey.Values.SingleOrDefault(t => t.ValueName == "Language")?.ValueData;
+
+                        var sourceList = subKey.SubKeys.SingleOrDefault(t => t.KeyName == "SourceList");
+                        string packageName = null;
+                        string lastUsedSource = null;
+
+                        if (sourceList != null)
+                        {
+                            packageName = sourceList.Values.SingleOrDefault(t => t.ValueName == "PackageName")?.ValueData;
+                            lastUsedSource = sourceList.Values.SingleOrDefault(t => t.ValueName == "LastUsedSource")?.ValueData;
+                        }
+                        DateTimeOffset? ts = subKey.LastWriteTime;
+
+                        var ff = new ValuesOut(productName, packageName, null, null, language, null, lastUsedSource, null, ts)
+                        {
+                            BatchValueName = "Multiple",
+                            BatchKeyPath = subKey.KeyPath
+                        };
+                        l.Add(ff);
+                    }
+                    catch (Exception ex)
+                    {
+                        Errors.Add($"Error processing Products key: {ex.Message}");
+                    }
                 }
-                catch (Exception ex)
+            }
+            else
+            {
+                foreach (var subKey in key.SubKeys)
                 {
-                    Errors.Add($"Error processing Products key: {ex.Message}");
+                    try
+                    {
+                        var properties = subKey.SubKeys.SingleOrDefault(t => t.KeyName == "InstallProperties");
+                        if (properties == null)
+                            continue;
+
+                        var displayName = properties.Values.SingleOrDefault(t => t.ValueName == "DisplayName")?.ValueData;
+                        var packageName = properties.Values.SingleOrDefault(t => t.ValueName == "DisplayVersion")?.ValueData;
+                        var installDate = properties.Values.SingleOrDefault(t => t.ValueName == "InstallDate")?.ValueData;
+                        var publisher = properties.Values.SingleOrDefault(t => t.ValueName == "Publisher")?.ValueData;
+                        var language = properties.Values.SingleOrDefault(t => t.ValueName == "Language")?.ValueData;
+                        var installLocation = properties.Values.SingleOrDefault(t => t.ValueName == "InstallLocation")?.ValueData;
+                        var installSource = properties.Values.SingleOrDefault(t => t.ValueName == "InstallSource")?.ValueData;
+                        var comments = properties.Values.SingleOrDefault(t => t.ValueName == "Comments")?.ValueData;
+                        DateTimeOffset? ts = properties.LastWriteTime;
+
+                        var ff = new ValuesOut(displayName, packageName, installDate, publisher, language, installLocation, installSource, comments, ts)
+                        {
+                            BatchValueName = "Multiple",
+                            BatchKeyPath = subKey.KeyPath
+                        };
+                        l.Add(ff);
+                    }
+                    catch (Exception ex)
+                    {
+                        Errors.Add($"Error processing Products key: {ex.Message}");
+                    }
                 }
             }
 
